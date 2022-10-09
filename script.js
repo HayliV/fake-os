32b9093fe307ffe32da5aaf35df0ab17ceaa6235
Hayli Verrier <hayliverrier@gmail.com>
2022-10-09 21:15:40 +1030

    added first app, embeded video

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+// toggling start menu
+
 const open = document.getElementById("open");
 const start_container = document.getElementById("start_container");
-const close = document.getElementById("close");
 
 open.addEventListener("click", () => {
     start_container.classList.toggle("show");
 });
 
-close.addEventListener("click", () => {
-    start_container.classList.remove("show");
+// opening Age of Empires App
+
+const startAoE = document.getElementById("startAoE");
+const AoE_container = document.getElementById("AoE_container");
+
+startAoE.addEventListener("click", () => {
+    AoE_container.classList.toggle("start");
 });
